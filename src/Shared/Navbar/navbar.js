bba4b6e94ab15901265a0f56c828d58d4b9f36d1
sureshkumara <sureshkumara6868@gmail.com>
2023-06-09 11:43:27 +0530

    responsive fix 2

--- src/Shared/Navbar/navbar.js
+++ src/Shared/Navbar/navbar.js
@@ -1,23 +1,25 @@
-
-import * as React from 'react';
+import React, { useState } from 'react';
 
 import './navbar.css'
 import car from "../../assests/images/car.png"
 import toggle from "../../assests/images/toggle.png"
 import { navigation } from "../Datas/Datas"
 export default function Navbar() {
+    const [navshow, setNavshow] = useState(false);
 
-
+    const shohideNav = () =>{
+        setNavshow(!navshow)
+    }
     return (
         <div>
             <div className='container'>
                 <ul className='topnav'>
                     <li><img src={car} alt={"logo"} /></li>
                     <li> TowTrackr </li>
-                    <li><img src={toggle} alt={"toggle menu"} /></li>
+                    <li onClick={shohideNav}><img src={toggle} alt={"toggle menu"} /></li>
                 </ul>
 
-                <ul className='navbar'>
+                <ul className={`navbar ${navshow ? "show" :"hide"}`}>
                     {navigation && navigation.map((item,index)=>{
                         return <li className={index ===0 ? "selected" : ""}> <img src={`http://localhost:3000/images/${item.url}.png`} alt={item.name}/> <a> {item.name} </a></li>
                     })}
